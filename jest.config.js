f4fd5a6dbfbf54cea9ca4c30d27b17baceb6e50e
Nathalia Pavan <nathaliapavans@pop-os.local>
2024-03-31 16:08:48 -0300

    chore: add libs for testing

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    "/src/infrastructure/database",
+    "/src/infrastructure/ioc",
+    "/src/infrastructure/swagger"
+  ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
+  
+};
